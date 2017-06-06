0bea1960c6be4d3d43bb226d3cc23839ecd37924
Kota Weaver <kweaver@bostonic.net>
2017-06-06 13:46:47 -0400

    remove unnecessary

--- setup.py
+++
@@ -1,6 +0,0 @@
-setup(name='Example', version='1.0',
-      description='using ajax button',
-      author='Kota Weaver',
-      author_email='kweaver@bostonic.net',
-      install_requires=['flask']
-)
